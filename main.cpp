a1f4139e7a0f5787587004ee770493fc489f3fdc
jamiemlawrence <jameslawrence557@gmail.com>
2024-10-14 17:27:12 -0700

    Basic outline

---
+++ main.cpp
@@ -0,0 +1,9 @@
+
+// class to respresent each individual task
+// class Task {}
+
+// class to represent a collection of tasks that belong to a single project
+// class Project {}
+
+// class to handle the overall management of different projects
+// class TaskManager {}
